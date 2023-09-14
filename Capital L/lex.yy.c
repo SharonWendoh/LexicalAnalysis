2737a9e3afeb2b9242459cb7b0bce9e4dbbb9c1c
SharonWendoh <swendoh.sharon@gmail.com>
2023-09-14 23:01:20 +0300

    Added part 2

--- Capital L/lex.yy.c
+++ Capital L/lex.yy.c
@@ -282,11 +282,11 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[9] =
+#define YY_NUM_RULES 6
+#define YY_END_OF_BUFFER 7
+static yyconst short int yy_accept[11] =
     {   0,
-        0,    0,    5,    2,    3,    1,    1,    0
+        0,    0,    7,    4,    5,    1,    2,    3,    2,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -294,17 +294,17 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    3,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    3,    1,    3,    1,    4,    4,    4,
+        4,    4,    4,    4,    4,    4,    4,    1,    3,    1,
+        1,    1,    3,    1,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
+        5,    5,    5,    5,    5,    5,    5,    5,    5,    5,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
 
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -321,29 +321,33 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[4] =
+static yyconst int yy_meta[6] =
     {   0,
-        1,    1,    2
+        1,    1,    1,    2,    1
     } ;
 
-static yyconst short int yy_base[10] =
+static yyconst short int yy_base[12] =
     {   0,
-        0,    0,    5,    6,    6,    0,    0,    6,    2
+        0,    0,    7,    8,    8,    8,    0,    8,    0,    8,
+        4
     } ;
 
-static yyconst short int yy_def[10] =
+static yyconst short int yy_def[12] =
     {   0,
-        8,    1,    8,    8,    8,    9,    9,    0,    8
+       10,    1,   10,   10,   10,   10,   11,   10,   11,    0,
+       10
     } ;
 
-static yyconst short int yy_nxt[10] =
+static yyconst short int yy_nxt[14] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    5,    6,    7,    8,    9,   10,    3,   10,   10,
+       10,   10,   10
     } ;
 
-static yyconst short int yy_chk[10] =
+static yyconst short int yy_chk[14] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,    1,    1,   11,    3,   10,   10,   10,
+       10,   10,   10
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -357,13 +361,18 @@ static char *yy_last_accepting_cpos;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "sentence_tokenizer.l"
+#line 1 "capital.l"
 #define INITIAL 0
-/*** Definition Section ***/
-#line 3 "sentence_tokenizer.l"
-#include <stdio.h>
-/*** Rule Section ***/
-#line 367 "lex.yy.c"
+/*** Definition Section has one variable
+which can be accessed inside yylex()
+and main() ***/
+#line 5 "capital.l"
+int count = 0;
+/*** Rule Section has three rules, first rule
+matches with capital letters, second rule
+matches with any character except newline and
+third rule does not take input after the enter***/
+#line 376 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -514,9 +523,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 7 "sentence_tokenizer.l"
+#line 12 "capital.l"
 
-#line 520 "lex.yy.c"
+
+#line 530 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -567,13 +577,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 11 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 8 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -601,25 +611,40 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 8 "sentence_tokenizer.l"
-{ printf("%s\n", yytext); }
+#line 14 "capital.l"
+{
+    printf("Punctuation: %s\n", yytext);
+}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 9 "sentence_tokenizer.l"
-; /* Ignore any other characters */
+#line 17 "capital.l"
+{
+    printf("This is a number -> %s\n", yytext);
+} 
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 10 "sentence_tokenizer.l"
-; /* Ignore newline characters */
+#line 20 "capital.l"
+{printf("%s capital letter\n", yytext);
+       count++;}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 11 "sentence_tokenizer.l"
+#line 22 "capital.l"
+{printf("%s not a capital letter or a number\n", yytext);}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 25 "capital.l"
+{return 0;}
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
+#line 27 "capital.l"
 ECHO;
 	YY_BREAK
-#line 623 "lex.yy.c"
+#line 648 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -911,7 +936,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 11 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -946,11 +971,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 11 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 10);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1505,13 +1530,34 @@ int main()
 	return 0;
 	}
 #endif
-#line 11 "sentence_tokenizer.l"
+#line 27 "capital.l"
 
 
-/*** Code Section ***/
-int yywrap() {}
-
-int main() {
-    yylex();
-    return 0;
+/*** Code Section prints the number of
+capital letter present in the given input***/
+int yywrap(){}
+int main(){
+ 
+// Explanation:
+// yywrap() - wraps the above rule section
+/* yyin - takes the file pointer
+          which contains the input*/
+/* yylex() - this is the main flex function
+          which runs the Rule Section*/
+// yytext is the text in the buffer
+ 
+// Uncomment the lines below
+// to take input from file
+// FILE *fp;
+// char filename[50];
+// printf("Enter the filename: \n");
+// scanf("%s",filename);
+// fp = fopen(filename,"r");
+// yyin = fp;
+ 
+yylex();
+printf("\nNumber of Captial letters "
+      "in the given input - %d\n", count);
+ 
+return 0;
 }
